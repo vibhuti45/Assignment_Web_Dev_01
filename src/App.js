a6e09a7b375840d5fb4aff4c228cbb593de87cb1
[Alok Kumar] <alokkumar@Aloks-MacBook-Air.local>
2022-07-26 15:09:16 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Jumbotron/>
+    <Form/>
+    </>
+  );
+}
+
+export default App;
